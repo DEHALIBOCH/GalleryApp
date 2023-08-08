6852544fa7c33b5ace90f80c8f20ee762bf622aa
Marat <almabekov.marat@gmail.com>
2023-08-08 18:34:30 +0600

    Добавление библеотек для приложения

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -41,10 +41,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    // OkHttp
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
-
     // RxJava
     implementation("io.reactivex.rxjava3:rxjava:3.1.5")
     implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
