34d8f2f6f4e191c4a3bf900faea2b456eef207f6
Marat <almabekov.marat@gmail.com>
2023-08-08 18:32:22 +0600

    Добавление библеотек для приложения

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -50,5 +50,6 @@ dependencies {
     val retrofitVersion = "2.9.0"
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
     implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    implementation("com.squareup.retrofit2:adapter-rxjava3:$retrofitVersion")
 
 }
