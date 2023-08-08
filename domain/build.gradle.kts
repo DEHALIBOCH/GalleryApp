8d9525962abb5942877ee696a014e74197211f58
Marat <almabekov.marat@gmail.com>
2023-08-08 18:34:21 +0600

    Добавление библеотек для приложения

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -45,12 +45,6 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
 
-    // Retrofit
-    val retrofitVersion = "2.9.0"
-    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
-    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
-    implementation("com.squareup.retrofit2:adapter-rxjava3:$retrofitVersion")
-
     // RxJava
     implementation("io.reactivex.rxjava3:rxjava:3.1.5")
     implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
