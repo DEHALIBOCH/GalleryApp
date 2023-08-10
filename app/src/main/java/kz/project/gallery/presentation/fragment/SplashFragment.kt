4bb9c6a4ee33b3dc6c0cd597513e338bb02b0cbe
Marat <almabekov.marat@gmail.com>
2023-08-10 19:05:38 +0600

    Настройка анимаций

--- app/src/main/java/kz/project/gallery/presentation/fragment/SplashFragment.kt
+++ app/src/main/java/kz/project/gallery/presentation/fragment/SplashFragment.kt
@@ -1,13 +1,43 @@
 package kz.project.gallery.presentation.fragment
 
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.viewbinding.library.fragment.viewBinding
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import kz.project.gallery.R
+import kz.project.gallery.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
+
+    private val binding: FragmentSplashBinding by viewBinding()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupAnimations()
+
+        // TODO доработка функционала, убрать addToBackStack
+        binding.logo.setOnClickListener {
+            parentFragmentManager.commit {
+                setReorderingAllowed(true)
+                addSharedElement(binding.logo, "logo_destination")
+                replace<WelcomeFragment>(R.id.mainActivityFragmentContainerView)
+                addToBackStack(null)
+            }
+        }
+    }
+
+    private fun setupAnimations() = binding.apply {
+        logo.startAnimation(
+            AnimationUtils.loadAnimation(requireContext(), R.anim.fade_in_animaton_1000)
+        )
+    }
 
 }
