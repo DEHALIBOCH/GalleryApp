a08e7b8f5141c02be9e953086965c3fde27100f0
Marat <almabekov.marat@gmail.com>
2023-08-10 17:59:33 +0600

    Начало работы над навигацией

--- app/src/main/java/kz/project/gallery/presentation/MainActivity.kt
+++ app/src/main/java/kz/project/gallery/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package kz.project.gallery.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.viewbinding.library.activity.viewBinding
+import androidx.fragment.app.commit
 import kz.project.gallery.R
 import kz.project.gallery.databinding.ActivityMainBinding
 import kz.project.gallery.presentation.fragment.CreatePhotoFragment
@@ -10,18 +11,17 @@ import kz.project.gallery.presentation.fragment.HomeFragment
 import kz.project.gallery.presentation.fragment.MainFragment
 import kz.project.gallery.presentation.fragment.SignInFragment
 import kz.project.gallery.presentation.fragment.SignUpFragment
+import kz.project.gallery.presentation.fragment.SplashFragment
 import kz.project.gallery.presentation.fragment.WelcomeFragment
 
 class MainActivity : AppCompatActivity() {
-
-    private val binding: ActivityMainBinding by viewBinding()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.mainActivityFragmentContainerView, CreatePhotoFragment())
-            .commit()
+        supportFragmentManager.commit {
+            replace(R.id.mainActivityFragmentContainerView, SplashFragment())
+        }
+
     }
 }
