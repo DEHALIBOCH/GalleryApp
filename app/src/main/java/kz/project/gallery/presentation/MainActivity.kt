2983f328dd1f5121d59ed57b4f3dfb8c229b087e
Marat <almabekov.marat@gmail.com>
2023-08-10 18:00:52 +0600

    Удаление не используемых импортов

--- app/src/main/java/kz/project/gallery/presentation/MainActivity.kt
+++ app/src/main/java/kz/project/gallery/presentation/MainActivity.kt
@@ -1,18 +1,10 @@
 package kz.project.gallery.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.viewbinding.library.activity.viewBinding
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import kz.project.gallery.R
-import kz.project.gallery.databinding.ActivityMainBinding
-import kz.project.gallery.presentation.fragment.CreatePhotoFragment
-import kz.project.gallery.presentation.fragment.HomeFragment
-import kz.project.gallery.presentation.fragment.MainFragment
-import kz.project.gallery.presentation.fragment.SignInFragment
-import kz.project.gallery.presentation.fragment.SignUpFragment
 import kz.project.gallery.presentation.fragment.SplashFragment
-import kz.project.gallery.presentation.fragment.WelcomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
