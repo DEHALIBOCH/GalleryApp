01abd02b70e65388e3264aecf5af5a7c6dd814d1
Marat <almabekov.marat@gmail.com>
2023-08-10 18:01:15 +0600

    Удаление не используемых импортов

--- app/src/main/java/kz/project/gallery/presentation/fragment/WelcomeFragment.kt
+++ app/src/main/java/kz/project/gallery/presentation/fragment/WelcomeFragment.kt
@@ -2,9 +2,6 @@ package kz.project.gallery.presentation.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import kz.project.gallery.R
 
 class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
