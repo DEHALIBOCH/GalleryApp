a37914e1973440cf9edc8dd1d1a75be834317a3a
Marat <almabekov.marat@gmail.com>
2023-08-11 00:44:42 +0600

    Утилитарные классы

---
+++ app/src/main/java/kz/project/gallery/utils/Constants.kt
@@ -0,0 +1,20 @@
+package kz.project.gallery.utils
+
+object Constants {
+
+    /**
+     * Базовый URL API
+     */
+//    TODO переместить в BuildConfig
+    const val BASE_URL = "https://gallery.prod1.webant.ru/"
+
+
+    /**
+     * TAG для Shared Preferences
+     */
+    const val SHARED_PREFS_ACCESS_TOKEN_TAG = "access_token_shared_prefs"
+
+    const val UNEXPECTED_ERROR = "Unexpected error"
+
+    const val isAlreadyCreated = "isAlreadyCreated"
+}
