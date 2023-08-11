883172f28e20100bf03c2fd47fe0f1c440579912
Marat <almabekov.marat@gmail.com>
2023-08-11 12:05:54 +0600

    Дополнительный @Binds для ViewModel

--- app/src/main/java/kz/project/gallery/di/AppBindsModule.kt
+++ app/src/main/java/kz/project/gallery/di/AppBindsModule.kt
@@ -4,8 +4,9 @@ import androidx.lifecycle.ViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
-import kz.project.gallery.presentation.viewmodel.AccessTokenViewModel
+import kz.project.gallery.presentation.viewmodel.access_token.AccessTokenViewModel
 import kz.project.gallery.presentation.viewmodel.signin_signup.LoginViewModel
+import kz.project.gallery.presentation.viewmodel.user.CurrentUserViewModel
 
 @Module
 interface AppBindsModule {
@@ -15,6 +16,9 @@ interface AppBindsModule {
 
     @[Binds IntoMap ViewModelKey(AccessTokenViewModel::class)]
     fun accessTokenViewModel(accessTokenViewModel: AccessTokenViewModel): ViewModel
+
+    @[Binds IntoMap ViewModelKey(CurrentUserViewModel::class)]
+    fun currentUserViewModel(currentUserViewModel: CurrentUserViewModel): ViewModel
 
 
 }
