cd48c4239a208255705b0fc2607e3576efd10bab
Marat <almabekov.marat@gmail.com>
2023-08-11 12:09:22 +0600

    ViewModel для хранения модели текущего пользователя

---
+++ app/src/main/java/kz/project/gallery/presentation/viewmodel/user/CurrentUserViewModel.kt
@@ -0,0 +1,14 @@
+package kz.project.gallery.presentation.viewmodel.user
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+/**
+ * ViewModel для хранения модели текущего авторизованного пользователя
+ */
+class CurrentUserViewModel @Inject constructor(
+
+) : ViewModel() {
+
+
+}
