40e93e966b83fd6b9ef79900d3735f4cd27e9eeb
Marat <almabekov.marat@gmail.com>
2023-08-12 18:15:19 +0600

    Начало работы над фрагментом настроек

--- app/src/main/java/kz/project/gallery/presentation/fragment/SettingsFragment.kt
+++ app/src/main/java/kz/project/gallery/presentation/fragment/SettingsFragment.kt
@@ -1,15 +1,39 @@
 package kz.project.gallery.presentation.fragment
 
 import android.os.Bundle
+import android.view.View
+import android.viewbinding.library.fragment.viewBinding
 import androidx.fragment.app.Fragment
 import kz.project.gallery.R
+import kz.project.gallery.databinding.FragmentSettingsBinding
 
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
+    private val binding: FragmentSettingsBinding by viewBinding()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupButtons()
+    }
+
+    /**
+     * Настраивает функционал кнопок
+     */
+    private fun setupButtons() = binding.apply {
+        // TODO
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    companion object {
+        const val FRAGMENT_TAG = "SettingsFragment"
+    }
 }
