d9b48b0ee8ff3efc1e949ab17df83fb6a33b017f
Marat <almabekov.marat@gmail.com>
2023-08-13 22:11:39 +0600

    Inject для PhotoDetailsFragment

--- app/src/main/java/kz/project/gallery/di/AppComponent.kt
+++ app/src/main/java/kz/project/gallery/di/AppComponent.kt
@@ -5,6 +5,7 @@ import dagger.BindsInstance
 import dagger.Component
 import kz.project.gallery.presentation.fragment.HomeFragment
 import kz.project.gallery.presentation.fragment.MainFragment
+import kz.project.gallery.presentation.fragment.PhotoDetailsFragment
 import kz.project.gallery.presentation.fragment.PhotoListFragment
 import kz.project.gallery.presentation.fragment.SignInFragment
 import kz.project.gallery.presentation.fragment.SignUpFragment
@@ -23,6 +24,7 @@ interface AppComponent {
     fun inject(fragment: HomeFragment)
     fun inject(fragment: MainFragment)
     fun inject(fragment: PhotoListFragment)
+    fun inject(fragment: PhotoDetailsFragment)
 
     val viewModelFactory: MultiViewModelFactory
 
