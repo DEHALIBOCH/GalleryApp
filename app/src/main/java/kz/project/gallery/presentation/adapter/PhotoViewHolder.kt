92377207677ada1731f7ba676abfd3971d7e16df
Marat <almabekov.marat@gmail.com>
2023-08-14 14:35:28 +0600

    Доработка вьюхолдера

---
+++ app/src/main/java/kz/project/gallery/presentation/adapter/PhotoViewHolder.kt
@@ -0,0 +1,28 @@
+package kz.project.gallery.presentation.adapter
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.View
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.android.material.imageview.ShapeableImageView
+import kz.project.domain.model.photo.Photo
+import kz.project.gallery.R
+import kz.project.gallery.utils.Constants
+
+class PhotoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    fun bind(photo: Photo) {
+        val imageName = photo.image.name
+        val loadingUrl = "${Constants.IMAGE_LOADING_URL}$imageName"
+        val photoImageView = itemView.findViewById<ShapeableImageView>(R.id.photoImageView)
+        Glide.with(photoImageView)
+            .load(loadingUrl)
+            .placeholder(ColorDrawable(Color.TRANSPARENT))
+            .error(AppCompatResources.getDrawable(itemView.context, R.drawable.error_with_loading))
+            .fitCenter()
+            .into(photoImageView)
+    }
+
+}
