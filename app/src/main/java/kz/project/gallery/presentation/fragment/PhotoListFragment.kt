c99e21d20cfa6781a9950126f67a0e4a9e6a4eef
Marat <almabekov.marat@gmail.com>
2023-08-15 11:38:54 +0600

    Доработка фрагмента со списком фотографий: перенос пагинации с paging3 на RecyclerView-ScrollListener, установка SwipeRefreshLayout

--- app/src/main/java/kz/project/gallery/presentation/fragment/PhotoListFragment.kt
+++ app/src/main/java/kz/project/gallery/presentation/fragment/PhotoListFragment.kt
@@ -3,6 +3,7 @@ package kz.project.gallery.presentation.fragment
 import android.os.Bundle
 import android.view.View
 import android.viewbinding.library.fragment.viewBinding
+import android.widget.AbsListView
 import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -10,6 +11,7 @@ import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kz.project.domain.model.photo.Photo
 import kz.project.domain.use_case.photo.GetPhotosListUseCase
 import kz.project.gallery.GalleryApp
@@ -18,6 +20,7 @@ import kz.project.gallery.databinding.FragmentPhotoListBinding
 import kz.project.gallery.presentation.adapter.PhotoAdapter
 import kz.project.gallery.presentation.adapter.PhotoItemType
 import kz.project.gallery.presentation.viewmodel.home.HomeViewModel
+import kz.project.gallery.utils.Constants
 import kz.project.gallery.utils.Resource
 import javax.inject.Inject
 
@@ -50,41 +53,93 @@ class PhotoListFragment : Fragment(R.layout.fragment_photo_list) {
 
         observePhotosResult()
         setupRecyclerView()
+        setupSwipeRefresh()
 
         if (!isAlreadyLoaded) {
-            viewModel.getPhotos()
             isAlreadyLoaded = true
         }
+    }
+
+    private fun setupSwipeRefresh() = binding.swipeRefreshLayout.apply {
+        setOnRefreshListener { viewModel.refreshPhotos() }
+        setColorSchemeResources(R.color.mainPink)
+        setProgressBackgroundColorSchemeResource(R.color.grayLight)
     }
 
     private fun observePhotosResult() = viewModel.photosLiveData.observe(viewLifecycleOwner) { resource ->
         when (resource) {
 
             is Resource.Loading -> {
-                showProgressBar(true)
                 showErrorNotification(false)
             }
 
             is Resource.Error -> {
-                showProgressBar(false)
+                hideSwipeRefresh()
+                hideProgressBar()
                 showErrorNotification(true)
             }
 
             is Resource.Success -> {
-                showProgressBar(false)
+                hideSwipeRefresh()
+                hideProgressBar()
                 showErrorNotification(false)
-                photoAdapter.submitList(resource.data?.photos)
+                isLastPage = viewModel.photosPage == viewModel.maxPhotosPage
+                photoAdapter.submitList(resource.data)
+                if (isLastPage) removeRecyclerViewPadding()
             }
         }
     }
 
+    private fun hideSwipeRefresh() = binding.apply {
+        isLoading = false
+        swipeRefreshLayout.isRefreshing = false
+    }
+
+    private fun removeRecyclerViewPadding() = binding.recyclerView.setPadding(0, 0, 0, 0)
+
+    private var isLoading = false
+    private var isScrolling = false
+    private var isLastPage = false
+
+    private val recyclerViewScrollListener = object : RecyclerView.OnScrollListener() {
+
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+
+            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                isScrolling = true
+            }
+        }
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            val layoutManager = recyclerView.layoutManager as GridLayoutManager
+            val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+            val visibleItemsCount = layoutManager.childCount
+            val totalItemsCount = layoutManager.itemCount
+
+            val isNotLoadingAndNotLastPage = !isLoading && !isLastPage
+            val isAtLastItem = firstVisibleItemPosition + visibleItemsCount >= totalItemsCount
+            val isNotAtBeginning = firstVisibleItemPosition >= 0
+            val isTotalMoreThanVisible = totalItemsCount >= Constants.LIMIT
+            val shouldPaginate =
+                isNotLoadingAndNotLastPage && isAtLastItem && isNotAtBeginning && isTotalMoreThanVisible && isScrolling
+
+            if (shouldPaginate) {
+                isLoading = true
+                viewModel.getPagingPhotos()
+                isScrolling = false
+            }
+        }
+    }
 
     private fun showErrorNotification(flag: Boolean) {
         binding.loadingError.isVisible = flag
     }
 
-    private fun showProgressBar(flag: Boolean) {
-        binding.loadingProgressBar.isVisible = flag
+    private fun hideProgressBar() {
+        binding.loadingProgressBar.isVisible = false
     }
 
     private fun setupRecyclerView() {
@@ -92,8 +147,11 @@ class PhotoListFragment : Fragment(R.layout.fragment_photo_list) {
         photoAdapter.setOnItemClickListener { photo ->
             goToPhotoDetailsFragment(photo)
         }
-        binding.recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
-        binding.recyclerView.adapter = photoAdapter
+        binding.recyclerView.apply {
+            layoutManager = GridLayoutManager(requireContext(), 2)
+            adapter = photoAdapter
+            addOnScrollListener(recyclerViewScrollListener)
+        }
     }
 
     private fun goToPhotoDetailsFragment(photo: Photo) = requireActivity().supportFragmentManager.commit {
