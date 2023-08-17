c8610a6939c69c4d085eb11d065f4d67231de29a
Marat <almabekov.marat@gmail.com>
2023-08-17 15:19:32 +0600

    Доработка запроса обновления запроса

--- data/src/main/java/kz/project/data/remote/UserApi.kt
+++ data/src/main/java/kz/project/data/remote/UserApi.kt
@@ -1,10 +1,11 @@
 package kz.project.data.remote
 
 import kz.project.data.remote.dto.user.UserDto
-import kz.project.data.remote.dto.user.UserToReceiveDto
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Single
+import kz.project.data.remote.dto.error.error_model.UpdatePasswordErrorDto
 import kz.project.domain.model.user.UpdatePasswordForm
+import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.PUT
@@ -19,7 +20,8 @@ interface UserApi {
     fun getCurrentUser(): Single<UserDto>
 
     @PUT("/api/users/update_password/{id}")
-    fun updatePassword(@Path("id") id: Int, updatePasswordForm: UpdatePasswordForm): Single<UserToReceiveDto>
+    fun updatePassword(@Path("id") id: Int, @Body updatePasswordForm: UpdatePasswordForm): Single<UpdatePasswordErrorDto>
+
     @GET("/api/users/{id}")
     fun getUserById(@Path("id") id: Int): Single<UserDto>
 
