3ab3fac8b346093158475f1396864782e6147398
Marat <almabekov.marat@gmail.com>
2023-08-22 10:12:54 +0600

    Удаление не используемых импортов

--- app/src/main/java/kz/project/gallery/presentation/fragment/PhotoListFragment.kt
+++ app/src/main/java/kz/project/gallery/presentation/fragment/PhotoListFragment.kt
@@ -5,7 +5,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
